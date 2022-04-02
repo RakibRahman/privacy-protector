7829414383384e78320fed9731ff94a8c0473027
RakibRahman <md.rr.talukder@gmail.com>
2022-04-02 17:23:55 +0600

    removed redundant update  code

--- src/components/Vault/DashBoard/UserDetail.tsx
+++ src/components/Vault/DashBoard/UserDetail.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
+  Input,
+  FormLabel,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
   Flex,
   Text,
   Image,
@@ -10,21 +15,58 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
   useDisclosure,
+  Stack,
 } from "@chakra-ui/react";
 import { CollectionProps } from "../../../interfaces/vaultTypes";
 import { Link } from "react-router-dom";
 import { useFireStore } from "../../../hooks/useFireStore";
 
-import { FcGlobe, FcKey, FcReading, FcAbout } from "react-icons/fc";
-import UpdateData from "./UpdateData";
+import {
+  FcGlobe,
+  FcKey,
+  FcReading,
+  FcAbout,
+  FcInspection,
+} from "react-icons/fc";
+
+
+import { AddDataProps } from "../../../interfaces/vaultTypes";
 
 const UserDetail: React.FC<{ login: CollectionProps }> = ({ login }) => {
-  const { deleteUserLogin } = useFireStore();
+  const initFormValues = {
+    site: login.site,
+    username: login.username,
+    password: login.password,
+  };
+
+  const { deleteUserLogin, updateUserLogin } = useFireStore();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [formState, setFormState] = useState<AddDataProps>(initFormValues);
+  const [loading, setLoading] = useState<boolean>(false);
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setFormState({ ...formState, [e.target.name]: value });
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setLoading(true);
+    try {
+      await updateUserLogin(login.id, formState);
+    } catch {
+      console.log("Something Went Wrong");
+    } finally {
+      setFormState(initFormValues);
+      onClose();
+      setLoading(false);
+    }
+    // window.location.reload();
+  };
 
   return (
     <>
@@ -59,53 +101,55 @@ const UserDetail: React.FC<{ login: CollectionProps }> = ({ login }) => {
         </Text>
       </Box>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent bg="#CAEDE0">
-          <ModalHeader bg="#23B179" color="white">
-            Login Details
-          </ModalHeader>
+        <ModalContent>
+          <ModalHeader>Login Details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Flex flexDirection="column" gridGap="3">
-              <Flex gridGap="4" align="center">
-                <FcGlobe fontSize="2.5rem" />
-                <Box>
-                  <Heading size="sm" letterSpacing="1px">
-                    Website
-                  </Heading>
-                  <Link
-                    to={{
-                      pathname: `${
-                        login.site.includes("https://")
-                          ? login.site
-                          : `https://${login.site}`
-                      }`,
-                    }}
-                    target="_blank"
-                  >
-                    {login.site}
-                  </Link>
-                </Box>
-              </Flex>
-              <Flex gridGap="4" align="center">
-                <FcReading fontSize="2.5rem" />
-                <Box>
-                  <Heading size="sm" letterSpacing="1px">
-                    ID/UserName
-                  </Heading>
-                  <Text>{login.username}</Text>
-                </Box>
-              </Flex>
-              <Flex gridGap="4" align="center">
-                <FcKey fontSize="2.5rem" />
-                <Box>
-                  <Heading size="sm" letterSpacing="1px">
-                    Password
-                  </Heading>
-                  <Text>{login.password}</Text>
-                </Box>
-              </Flex>
+            <form onSubmit={onSubmit}>
+              <Stack spacing={4}>
+                <FormLabel>
+                  <Text my="1"> Site URL:</Text>
+                  <Input
+                    placeholder="Website Address"
+                    name="site"
+                    value={formState.site}
+                    onChange={onChangeHandler}
+                  />
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<FcGlobe color="gray.300" />}
+                  />
+                  <Input type="tel" placeholder="Phone number" />
+                  <InputRightElement
+                    onClick={()=>console.log('ok')}
+                    children={<FcInspection color="green.500" />}
+                  />
+                </InputGroup>
+                <FormLabel>
+                  <Text my="1">Username:</Text>
+
+                  <Input
+                    placeholder="Your Username or email"
+                    name="username"
+                    value={formState.username}
+                    onChange={onChangeHandler}
+                  />
+                </FormLabel>
+                <FormLabel>
+                  <Text my="1">Password:</Text>
+
+                  <Input
+                    placeholder="Password"
+                    name="password"
+                    value={formState.password}
+                    onChange={onChangeHandler}
+                  />
+                </FormLabel>
+              </Stack>
 
               <Flex gridGap="4" align="center">
                 <FcAbout fontSize="2.5rem" />
@@ -116,26 +160,36 @@ const UserDetail: React.FC<{ login: CollectionProps }> = ({ login }) => {
                   <Text>{login.createdAt.toDate().toDateString()}</Text>
                 </Box>
               </Flex>
-            </Flex>
+              <Stack align="center" justify="center">
+                <Button
+                  bg="#C73636"
+                  color="white"
+                  mx="1"
+                  onClick={() => deleteUserLogin(login.id)}
+                  _hover={{
+                    opacity: 0.7,
+                    transform: "scale(1.03)",
+                  }}
+                >
+                  Delete
+                </Button>
+                <Button
+                  isLoading={loading}
+                  loadingText="Saving..."
+                  type="submit"
+                  bg="#1AB188"
+                  color="#ffffff"
+                  fontSize="1.5rem"
+                  _hover={{
+                    background: "white",
+                    color: "teal.500",
+                  }}
+                >
+                  Save
+                </Button>
+              </Stack>
+            </form>
           </ModalBody>
-
-          <ModalFooter>
-            <Flex align="center" justify="center">
-              <Button
-                bg="#C73636"
-                color="white"
-                mx="1"
-                onClick={() => deleteUserLogin(login.id)}
-                _hover={{
-                  opacity: 0.7,
-                  transform: "scale(1.03)",
-                }}
-              >
-                Delete
-              </Button>
-              <UpdateData login={login} />
-            </Flex>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
